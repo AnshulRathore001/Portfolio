aa034e6783191d022f68e886cad51dcda4a9eab2
AnshulRathore001 <94286361+AnshulRathore001@users.noreply.github.com>
2023-11-14 21:31:46 +0530

    Add files via upload
    all

---
+++ Personal Portfolio/script.js
@@ -0,0 +1,60 @@
+// Sticky Navigation Menu JS Code
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+console.log(scrollBtn);
+let val;
+window.onscroll = function() {
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("sticky");
+    scrollBtn.style.display = "block";
+  }else{
+    nav.classList.remove("sticky");
+    scrollBtn.style.display = "none";
+  }
+
+}
+
+// Side NavIgation Menu JS Code
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = function(){
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflow = "hidden";
+  scrollBtn.style.pointerEvents = "none";
+}
+cancelBtn.onclick = function(){
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflow = "auto";
+  scrollBtn.style.pointerEvents = "auto";
+}
+getElemnt
+
+// Side Navigation Bar Close While We Click On Navigation Links
+let navLinks = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click" , function() {
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+  });
+  var button = document.getElementsByClassName("button");
+    button.onclick = function() {
+      var downloadLink = document.createElement("button");
+        
+      // Set the href attribute to the URL of the document to be downloaded
+      downloadLink.href = "Anshul CV.docx"; // Replace with the actual document URL
+      
+      
+      // Set the download attribute with the desired filename
+      downloadLink.download = "A"; // Replace with the desired filename
+      
+      // Trigger a click event on the anchor element to initiate the download
+      downloadLink.click();
+}
+}
